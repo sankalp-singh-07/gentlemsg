6441e8846e9b1256b314550868710c3b03bf4767
sankalp-singh-07 <sankalp.singh.somvanshi@gmail.com>
2024-06-06 00:34:28 +0530

    Sign In button styles added

--- src/components/home/home.component.jsx
+++ src/components/home/home.component.jsx
@@ -1,9 +1,29 @@
 import SignIn from "../auth/sign-in.component"
+import '../../styles/home.css'
 
 const Home = () => {
     return (
         <>
-            <h1>Home Page</h1>
+
+            <div>
+                <h1 className="logo">Gentle<span>.</span>MSG</h1>
+                <h1>CONTACT ME</h1>
+            </div>
+
+            <div>
+                <h1>Message</h1>
+                <h1>in Motion</h1>
+            </div>
+
+            <div>
+                <h3>One Click Away</h3>
+                <h3>Start Your Journey</h3>
+            </div>
+
+            <div>
+                <h4>Here every word propels you forward into fast and secure network of connections.</h4>
+            </div>
+
             <SignIn />
         </>
     )
