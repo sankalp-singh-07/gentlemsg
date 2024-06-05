a29ad327ea4be681f32b6e6542878e0837afb730
Sankalp <sankalp.singh.somvanshi@gmail.com>
2024-06-05 20:09:16 +0530

    Sign out component created and auto auth added to app.jsx

--- src/components/auth/sign-out.component.jsx
+++ src/components/auth/sign-out.component.jsx
@@ -1,7 +1,34 @@
+import { auth, db } from "../../utils/firebase"
+import { doc, serverTimestamp, updateDoc } from "firebase/firestore"
+import { signOut } from "firebase/auth"
+import { removeCookie } from "../../utils/cookies"
+
 const SignOut = () => {
+
+    const handleClick = async () => {
+
+        try {
+
+            const user = auth.currentUser;
+            if(user){
+                const userRef = doc(db, 'users', user.uid)
+                await updateDoc(userRef, {
+                    isOnline: false,
+                    lastActive: serverTimestamp()
+                })
+            }
+         
+            await signOut(auth);
+            removeCookie()
+
+        } catch (error) {
+            alert('Sign Out Error', error)
+        }
+    }
+
     return(
         <>
-            <h2>Sign Out</h2>
+            <button onClick={handleClick}>Sign Out</button>
         </>
     )
 }
