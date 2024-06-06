57f0745e4d92c894a54a1c1dd930f71c4800ab97
sankalp-singh-07 <sankalp.singh.somvanshi@gmail.com>
2024-06-06 13:36:58 +0530

    Home page styles added

--- src/components/home/home.component.jsx
+++ src/components/home/home.component.jsx
@@ -1,30 +1,37 @@
 import SignIn from "../auth/sign-in.component"
 import '../../styles/home.css'
+import { Link } from "react-router-dom"
 
 const Home = () => {
     return (
         <>
 
-            <div>
-                <h1 className="logo">Gentle<span>.</span>MSG</h1>
-                <h1>CONTACT ME</h1>
+            <div className="container-1">
+                <p className="logo">Gentle<span className="dot">.</span>MSG</p>
+                <Link className="contact" to='/s'>CONTACT ME</Link>
+            </div>
+
+            <div className="container">
+                <h1 className="heading text-7xl md:text-5xl lg:text-7xl pr-14 mt-24 mb-8 text-black font-bold">Message</h1>
+                <h1 className="heading text-7xl md:text-5xl lg:text-7xl pl-14 mt-24 mb-8 text-black font-bold">in <span className="text-primary px-5">Motion</span></h1>
+            </div>
+
+            <div className="container">
+                <h3 className="heading text-4xl md:text-2xl lg:text-4xl pr-10 mb-10 text-black font-semibold">One Click Away</h3>
+                <h3 className="heading text-4xl md:text-2xl lg:text-4xl pl-10 mb-10 text-primary font-semibold">Start Your Journey</h3>
             </div>
 
             <div>
-                <h1>Message</h1>
-                <h1>in Motion</h1>
-            </div>
-
-            <div>
-                <h3>One Click Away</h3>
-                <h3>Start Your Journey</h3>
-            </div>
-
-            <div>
-                <h4>Here every word propels you forward into fast and secure network of connections.</h4>
+                <h4 className="sub-heading">Here every word propels you forward into fast and secure network of connections.</h4>
             </div>
 
             <SignIn />
+
+            <div className="flex w-full h-52 space-x-6 items-end px-8">
+                <div className="flex-1 h-52 bg-secondary rounded-lg"></div>
+                <div className="flex-1 h-44 mt-4 bg-secondary rounded-lg"></div>
+                <div className="flex-1 h-52 bg-secondary rounded-lg"></div>
+            </div>
         </>
     )
 }
