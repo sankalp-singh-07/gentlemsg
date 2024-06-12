332b8aade1c98737e42807135556851bc4b401d4
sankalp-singh-07 <sankalp.singh.somvanshi@gmail.com>
2024-06-12 21:05:24 +0530

    dashboard design created

--- src/components/chat/chat.component.jsx
+++ src/components/chat/chat.component.jsx
@@ -2,7 +2,79 @@ import React from 'react';
 import './chat.css';
 
 const Chat = () => {
-	return <div className="chat">Chat</div>;
+	return (
+		<div className="chat">
+			<div className="top">
+				<div className="userDetails">
+					<img
+						src="src\assets\profile.jpg"
+						alt="profile"
+						className="avatar"
+					/>
+					<div className="currentStatus">
+						<span className="text-black font-semibold text-base">
+							John Doe
+						</span>
+						<p className="sub flex justify-between items-center bg-black py-0.5 px-2 rounded-xl">
+							Active
+							<span>
+								<img
+									src="src\assets\active.png"
+									className="w-2 h-2 ml-3"
+								/>
+							</span>
+						</p>
+					</div>
+				</div>
+				<div className="icons">
+					<img
+						src="src\assets\video.png"
+						alt="video"
+						className="w-8 h-8 mr-6"
+					/>
+					<img
+						src="src\assets\search.png"
+						alt="video"
+						className="w-6 h-6 mr-6"
+					/>
+					<img
+						src="src\assets\dots.png"
+						alt="video"
+						className="w-6 h-6 mr-4"
+					/>
+				</div>
+			</div>
+			<div className="mid"></div>
+			<div className="bottom">
+				<div className="inputContainer">
+					<div className="inputEl">
+						<img
+							src="src\assets\happy.png"
+							alt="emoji"
+							className="w-6 h-6 ml-2 cursor-pointer"
+						/>
+						<input
+							type="text"
+							placeholder="Type a message"
+							className="w-full h-full outline-none px-4 m-3 bg-quatery"
+						/>
+						<img
+							src="src\assets\folder.png"
+							alt="mic"
+							className="w-6 h-6 mr-2 cursor-pointer"
+						/>
+					</div>
+					<div className="bg-quatery rounded-xl p-4 drop-shadow">
+						<img
+							src="src\assets\send.png"
+							alt="send"
+							className="w-8 h-8 cursor-pointer"
+						/>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Chat;
