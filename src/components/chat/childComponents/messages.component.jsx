b5bfd98d8ea45115e7bf8cc703f8baefe2d09288
sankalp-singh-07 <sankalp.singh.somvanshi@gmail.com>
2024-06-13 14:18:16 +0530

    message section designed

---
+++ src/components/chat/childComponents/messages.component.jsx
@@ -0,0 +1,95 @@
+import React from 'react';
+
+const Messages = () => {
+	return (
+		<>
+			<div className="message">
+				<img
+					src="src\assets\profile.jpg"
+					className="w-8 h-8 m-3 rounded-full"
+				/>
+				<div className="texts">
+					<span className="textContent text-left">
+						shefhk jhsbfjk ejhfbjh jheefbjhs fvvfwh fwfewe fb fbwjf
+						jw fweevfnew efvewf jewf jwf jwef hweef hwfi whfhwvfj
+						wefhu
+					</span>
+					<span className="flex self-end text-sm font-normal">
+						1 min ago
+					</span>
+				</div>
+			</div>
+			<div className="message own">
+				<div className="texts">
+					<p className="textContent text-right">
+						shefhk jhsbfjk ejhfbjh jheefbjhs fvvfwh fwfewe fb fbwjf
+						jw fweevfnew efvewf jewf jwf jwef hweef hwfi whfhwvfj
+						wefhu
+					</p>
+					<span className="flex self-end text-sm font-normal">
+						1 min ago
+					</span>
+				</div>
+			</div>
+			{/* More Goes Here */}
+			<div className="message">
+				<img
+					src="src\assets\profile.jpg"
+					className="w-8 h-8 m-3 rounded-full"
+				/>
+				<div className="texts">
+					<span className="textContent text-left">
+						shefhk jhsbfjk ejhfbjh jheefbjhs fvvfwh fwfewe fb fbwjf
+						jw fweevfnew efvewf jewf jwf jwef hweef hwfi whfhwvfj
+						wefhu
+					</span>
+					<span className="flex self-end text-sm font-normal">
+						1 min ago
+					</span>
+				</div>
+			</div>
+			<div className="message own">
+				<div className="texts">
+					<p className="textContent text-right">
+						shefhk jhsbfjk ejhfbjh jheefbjhs fvvfwh fwfewe fb fbwjf
+						jw fweevfnew efvewf jewf jwf jwef hweef hwfi whfhwvfj
+						wefhu
+					</p>
+					<span className="flex self-end text-sm font-normal">
+						1 min ago
+					</span>
+				</div>
+			</div>
+			<div className="message">
+				<img
+					src="src\assets\profile.jpg"
+					className="w-8 h-8 m-3 rounded-full"
+				/>
+				<div className="texts">
+					<span className="textContent text-left">
+						shefhk jhsbfjk ejhfbjh jheefbjhs fvvfwh fwfewe fb fbwjf
+						jw fweevfnew efvewf jewf jwf jwef hweef hwfi whfhwvfj
+						wefhu
+					</span>
+					<span className="flex self-end text-sm font-normal">
+						1 min ago
+					</span>
+				</div>
+			</div>
+			<div className="message own">
+				<div className="texts">
+					<p className="textContent text-right">
+						shefhk jhsbfjk ejhfbjh jheefbjhs fvvfwh fwfewe fb fbwjf
+						jw fweevfnew efvewf jewf jwf jwef hweef hwfi whfhwvfj
+						wefhu
+					</p>
+					<span className="flex self-end text-sm font-normal">
+						1 min ago
+					</span>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default Messages;
