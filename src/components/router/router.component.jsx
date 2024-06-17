5deef6232a3ad363df9d56b0c82d4ecf2fab6437
Sankalp <sankalp.singh.somvanshi@gmail.com>
2024-06-17 11:39:26 +0530

    added a few little adjustments and made chat dashboard responsive

--- src/components/router/router.component.jsx
+++ src/components/router/router.component.jsx
@@ -1,38 +1,41 @@
-import { Routes, Route, Navigate, BrowserRouter } from "react-router-dom"
-import Home from "../home/home.component"
-import Admin from "../admin/admin.component"
-import NotFound from "../404/notfound.component"
-import { useAuthState } from 'react-firebase-hooks/auth'
-import { auth } from "../../utils/firebase"
-import { useEffect } from "react"
+import { Routes, Route, Navigate, BrowserRouter } from "react-router-dom";
+import Home from "../home/home.component";
+import Admin from "../admin/admin.component";
+import NotFound from "../404/notfound.component";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../../utils/firebase";
+import { useEffect } from "react";
+import Chat from "../chat/chat.component";
 
 const Router = () => {
+  const [user, loading, error] = useAuthState(auth);
 
-    const [user, loading, error] = useAuthState(auth);
-
-    useEffect(() => {
-        if(error) return(
-            <>
-                <h1>Error</h1>
-            </>
-        )
-    },[error])
-
-    if(loading) return(
+  useEffect(() => {
+    if (error)
+      return (
         <>
-            <h1>Loading</h1>
+          <h1>Error</h1>
         </>
-    )
+      );
+  }, [error]);
 
+  if (loading)
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={user ? < Navigate to="/admin" /> : <Home />} />
-                <Route path="/admin" element={user ? <Admin /> : <Navigate to="/" />} />
-                <Route path="*" element={<NotFound />} />
-            </Routes>
-        </BrowserRouter>
-    )
-}
+      <>
+        <h1>Loading</h1>
+      </>
+    );
 
-export default Router
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={user ? <Navigate to="/admin" /> : <Home />} />
+        <Route path="/admin" element={user ? <Admin /> : <Navigate to="/" />} />
+        <Route path="*" element={<NotFound />} />
+        <Route path="/chat" element={<Chat inMobile="show" />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{js,jsx, css}"],
-  darkMode: false, 
+  darkMode: false,
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter', 'sans-serif'],
+        sans: ["Inter", "sans-serif"],
       },
       colors: {
         primary: "#0077B6",
         secondary: "#0081A7",
         tertiary: "#F4F9FF",
         quatery: "#DBEBFF",
-        black: '#01161E',
+        black: "#01161E",
       },
     },
   },
-  
-  plugins: []
-}
+
+  plugins: [],
+};
