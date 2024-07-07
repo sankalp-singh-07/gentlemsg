87359ea77f442ddd8e9ab8ed5c4a8fd212232254
sankalp-singh-07 <sankalp.singh.somvanshi@gmail.com>
2024-07-07 14:33:41 +0530

    Emoji picker added to the chats component

--- src/components/chat/chat.component.jsx
+++ src/components/chat/chat.component.jsx
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './chat.css';
 import Messages from './childComponents/messages.component';
+import EmojiPicker from 'emoji-picker-react';
 
 const Chat = ({ inMobile }) => {
+	const [emojiPickerOpen, setEmojiPickerOpen] = useState(false);
+	const [text, setText] = useState('');
+	const textBoxRef = useRef(null);
+	const lastMessageShowRef = useRef(null);
+
+	useEffect(() => {
+		textBoxRef.current.focus();
+		lastMessageShowRef.current?.scrollIntoView({ behavior: 'smooth' });
+	}, []);
+
+	const handleEmoji = (e) => {
+		setText(text + e.emoji);
+		setEmojiPickerOpen(false);
+		textBoxRef.current.focus();
+	};
+
+	console.log(text);
+
 	return (
 		<div
 			className={`chat ${inMobile === 'hidden' && 'max-[650px]:hidden'}`}
@@ -49,19 +68,35 @@ const Chat = ({ inMobile }) => {
 			</div>
 			<div className="middle scrollbar-hide">
 				<Messages />
+				<div ref={lastMessageShowRef}></div>
 			</div>
 			<div className="bottom mt-2">
 				<div className="inputContainer">
 					<div className="inputEl">
-						<img
-							src="src\assets\happy.png"
-							alt="emoji"
-							className="w-6 h-6 ml-2 cursor-pointer"
-						/>
+						<div className="relative">
+							<img
+								src="src\assets\happy.png"
+								alt="emoji"
+								className="w-6 h-6 ml-2 cursor-pointer"
+								onClick={() =>
+									setEmojiPickerOpen(!emojiPickerOpen)
+								}
+							/>
+							<div className="absolute bottom-14 -left-8">
+								<EmojiPicker
+									open={emojiPickerOpen}
+									onEmojiClick={handleEmoji}
+									width="93%"
+								/>
+							</div>
+						</div>
 						<input
 							type="text"
 							placeholder="Type a message"
 							className="w-full h-full outline-none px-4 md:m-3 bg-quatery"
+							onChange={(e) => setText(e.target.value)}
+							value={text}
+							ref={textBoxRef}
 						/>
 						<img
 							src="src\assets\folder.png"
