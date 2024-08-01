143fef867f52662645f10d1b74c94592edf9b9e5
sankalp-singh-07 <sankalp.singh.somvanshi@gmail.com>
2024-08-01 22:13:47 +0530

    smooth scrolling in the messages, sorted chats so latest will come up and message component set up to show the messages between chats

--- src/components/chat/childComponents/messages.component.jsx
+++ src/components/chat/childComponents/messages.component.jsx
@@ -1,46 +1,72 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useRef, useState } from 'react';
 import { MessageContext } from '../../../context/message.context';
 import { useSelector } from 'react-redux';
 import { selectCurrentUser } from '../../../store/user/user.selector';
+import { decryptMessage, generateKey } from '../../../utils/encryption';
 
-const Messages = () => {
+const Messages = ({ receiverImg }) => {
 	const { messages, chatId } = useContext(MessageContext);
-	// console.log(messages);
+	const messagesArr = messages.messages || [];
+
 	const { currentUser } = useSelector(selectCurrentUser);
+	const { receiverInfo, setReceiverInfo } = useState([]);
 	const receiverId = chatId
 		.split('-')
 		.filter((el) => el !== currentUser.id)[0];
 
+	// Scroll
+	const messagesEndRef = useRef(null);
+
+	useEffect(() => {
+		messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+	}, [messagesArr]);
+
+	// Helper functions
+	const getDate = (timeStamp) => {
+		const date = new Date(timeStamp);
+		const options = { day: 'numeric', month: 'short' };
+		return new Intl.DateTimeFormat('en-US', options).format(date);
+	};
+
+	const showDecryptedMessage = (message, chatId) => {
+		const userIds = chatId.split('-');
+
+		const encryptionKey = generateKey(userIds[0], userIds[1]);
+		const decryptedMessage = decryptMessage(message, encryptionKey);
+		if (!decryptedMessage) {
+			console.error(`Failed to decrypt message: ${message}`);
+			return 'Failed to get message';
+		}
+		return decryptedMessage;
+	};
+
 	return (
 		<>
-			<div className="message">
-				<img
-					src="src\assets\profile.jpg"
-					className="w-8 h-8 m-3 rounded-full"
-				/>
-				<div className="texts">
-					<span className="textContent text-left">
-						shefhk jhsbfjk ejhfbjh jheefbjhs fvvfwh fwfewe fb fbwjf
-						jw fweevfnew efvewf jewf jwf jwef hweef hwfi whfhwvfj
-						wefhu
-					</span>
-					<span className="flex self-end text-sm font-normal">
-						1 min ago
-					</span>
-				</div>
-			</div>
-			<div className="message own">
-				<div className="texts">
-					<p className="textContent text-right">
-						shefhk jhsbfjk ejhfbjh jheefbjhs fvvfwh fwfewe fb fbwjf
-						jw fweevfnew efvewf jewf jwf jwef hweef hwfi whfhwvfj
-						wefhu
-					</p>
-					<span className="flex self-end text-sm font-normal">
-						1 min ago
-					</span>
-				</div>
-			</div>
+			{messagesArr.map((message, index) => {
+				const isOwn = message.senderId === currentUser.id;
+				return (
+					<div
+						key={index}
+						className={`message ${isOwn ? 'own' : ''}`}
+					>
+						{!isOwn && (
+							<img
+								src={receiverImg}
+								className="w-8 h-8 m-3 rounded-full"
+							/>
+						)}
+						<div className="texts">
+							<span className="textContent text-left">
+								{showDecryptedMessage(message.message, chatId)}
+							</span>
+							<span className="flex self-end text-sm font-normal">
+								{getDate(message.sentAt)}
+							</span>
+						</div>
+						<div ref={messagesEndRef} />
+					</div>
+				);
+			})}
 		</>
 	);
 };
