6143c5a3c427ba9bb55c7af39c09a25d3f183e24
sankalp-singh-07 <sankalp.singh.somvanshi@gmail.com>
2024-08-20 22:36:23 +0530

    showing all media sent in a chat at one place in media component created

--- src/components/chat/childComponents/media.component.jsx
+++ src/components/chat/childComponents/media.component.jsx
@@ -1,22 +1,85 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { DialogContext } from '../../../context/dialog.context';
+import { MessageContext } from '../../../context/message.context';
+import { getDownloadURL, getMetadata, listAll, ref } from 'firebase/storage';
+import { storage } from '../../../utils/firebase';
+import pdfIcon from '../../../assets/pdf-file.png';
 
 const Media = () => {
+	const { chatId } = useContext(MessageContext);
 	const { setOpenMediaDialog } = useContext(DialogContext);
+	const [mediaData, setMediaData] = useState([]);
+
+	useEffect(() => {
+		const fetchFiles = async () => {
+			const listRef = ref(storage, `chats/${chatId}`);
+
+			try {
+				const res = await listAll(listRef);
+				const data = await Promise.all(
+					res.items.map(async (itemRef) => {
+						const url = await getDownloadURL(itemRef);
+						const metadata = await getMetadata(itemRef);
+						return { url, contentType: metadata.contentType };
+					})
+				);
+				setMediaData(data);
+			} catch (error) {
+				console.log('Error fetching media files', error);
+			}
+		};
+
+		fetchFiles();
+	}, [chatId]);
 
 	return (
-		<>
-			<div className="bg-secondary md:w-6/12 w-10/12 h-96 absolute m-auto top-0 right-0 bottom-0 left-0 shadow-md">
-				<div>
-					<p>Content</p>
-				</div>
-				<div>
-					<button onClick={() => setOpenMediaDialog(false)}>
-						Close
-					</button>
-				</div>
+		<div className="bg-secondary  max-md:w-11/12 max-lg:w-9/12 w-6/12 h-5/6  m-auto top-0 right-0 bottom-0 left-0 overflow-scroll absolute shadow-2xl">
+			<div className="grid grid-cols-3 grid-rows-3">
+				{mediaData.map((media, index) => {
+					return (
+						<div key={index}>
+							{media.contentType.includes('image') ? (
+								<img
+									src={media.url}
+									alt={`media-${index}`}
+									className="w-10/12 h-10/12 m-5"
+									onClick={() => window.open(media.url)}
+								/>
+							) : media.contentType.includes('video') ? (
+								<video controls>
+									<source src={media.url} type="video/mp4" />
+								</video>
+							) : media.contentType === 'application/pdf' ? (
+								<a
+									href={media.url}
+									target="_blank"
+									rel="noopener noreferrer"
+									className="media-pdf text-inherit w-full h-full flex justify-center items-center"
+									style={{
+										backgroundImage: `url(${pdfIcon})`,
+										backgroundRepeat: 'no-repeat',
+										backgroundPosition: 'center',
+										backgroundSize: 'contain',
+									}}
+								>
+									...
+								</a>
+							) : (
+								<a></a>
+							)}
+						</div>
+					);
+				})}
 			</div>
-		</>
+			<div>
+				<button
+					className="w-full h-14 bg-quatery text-secondary font-semibold"
+					onClick={() => setOpenMediaDialog(false)}
+				>
+					Close
+				</button>
+			</div>
+		</div>
 	);
 };
 
